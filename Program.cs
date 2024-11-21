0b6483549e15d46dd19034e001f977feeade0913
unknown <albert.puertas@cirvianum.cat>
2024-11-21 13:05:04 +0100

    Fem el commit del Program.cs modificat valors modificats

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Model S", "Tesla", 2022, 250.0);
+        Vehicle vehicle1 = new Vehicle("Corolla", "Toyota", 2020, 240.0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Mustang", "Ford", 1965, 220.0);
+        Vehicle vehicle2 = new Vehicle("Aygo", "Toyota", 1990, 230.0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
