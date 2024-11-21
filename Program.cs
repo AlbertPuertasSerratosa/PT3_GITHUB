5d64063ec2656c98eb78e06bdecc50861da49e88
unknown <albert.puertas@cirvianum.cat>
2024-11-21 13:37:17 +0100

    Segona modificacio de marques de Program.cs

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Corolla", "Toyota", 2020, 240.0);
+        Vehicle vehicle1 = new Vehicle("Corolla", "Opel", 2020, 240.0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Aygo", "Toyota", 1990, 230.0);
+        Vehicle vehicle2 = new Vehicle("Aygo", "Opel", 1990, 230.0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
